372af67ccd3db08e6226e3d79320bfc8d1ae91ae
Iago Arantes <iagaopaarantes@gmail.com>
2025-04-23 15:20:23 -0300

    Boas vindas

---
+++ funcao2.py
@@ -0,0 +1,7 @@
+def boasVindas(nome):
+    print(f"Olá {nome}, seja bem-vindo!")
+
+nome2 = input("Digite seu nome: ")
+#Chamando a função
+boasVindas(nome2)
+boasVindas(input("Digite seu nome: "))    
