5c0230ed9b4e90dbcd35bb8ee5e108bfb754caaa
Iago Arantes <iagaopaarantes@gmail.com>
2025-04-23 15:55:02 -0300

    Calculadora

---
+++ Calculadora1.py
@@ -0,0 +1,22 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+
+#soma
+soma = num1 + num2
+print("A soma dos numeros é: " , soma)
+
+#Subtração 
+menos = num1 - num2
+print("O resultado da subtração é: " , menos)
+
+#Multiplicação
+mult = num1 * num2
+print("O resultado da multiplicação é:" , mult)
+
+#Divisão
+div = num1 / num2
+print("O resultado da divisão é: " , div)
+
+#Média
+media = (num1 +num2) / 2
+print("A média é:" , media)
